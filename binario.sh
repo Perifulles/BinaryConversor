fc79a13a96517f72732195f0de1bcf3aeca97a2e
Miguel Pérez <miguelperez@Beginners-Mac.local>
2025-03-27 20:43:28 +0100

    Funciones para pasar IPS realizadas, función 3 hecha a mano el 90% (1 hora para hacerla)  por desesperacion he hecho la 4 con ayuda de copilot

--- binario.sh
+++ binario.sh
@@ -44,6 +44,95 @@ adecimal() {
     echo "$binario convertido a decimal es: $suma";
 }
 
+ipabinario(){
+# Pedir la IP al usuario
+read -p "Ingrese una dirección IP en decimal:
+" ip
+
+    # Separar la IP en sus bytes
+    IFS='.' read -r byte1 byte2 byte3 byte4 <<< "$ip"
+
+    bytes=($byte1 $byte2 $byte3 $byte4)
+    ipfinal=()
+
+
+    for (( i=0; i<4; i++ ))
+    do
+        decimal=${bytes[$i]}
+
+        res=0;
+        binario="";
+
+        while [ $decimal -gt 0 ]; do
+        let mitad=( $decimal / 2 );
+        let sobrante=( $decimal % 2 );
+        decimal=$mitad;
+        binario="$sobrante$binario";
+        done
+        ipfinal+=("$binario")
+    done
+
+    for (( i=0; i<4; i++ ))
+    do
+    bite=${ipfinal[$i]}
+    longbite=${#bite};
+    suplemento=$(( 8 - $longbite ));
+
+        ceros=""
+        for (( o=0; o<$suplemento; o++ ))
+        do
+        ceros="0$ceros"
+        done
+
+    ipfinal[$i]="$ceros${ipfinal[$i]}"
+
+    ipcompleta="$ipcompleta.${ipfinal[$i]}"
+    done
+
+    ipbien=${ipcompleta:1:35};
+
+    echo "$ip en Binario es => $ipbien";
+}
+
+
+
+ipadecimal(){
+# Pedir la IP al usuario
+read -p "Ingrese una dirección IP en decimal:
+" ip
+
+    # Separar la IP en sus bytes
+    IFS='.' read -r byte1 byte2 byte3 byte4 <<< "$ip"
+
+    bytes=($byte1 $byte2 $byte3 $byte4)
+    ipfinal=()
+
+    for (( i=0; i<4; i++ ))
+    do
+        binario=${bytes[$i]}
+
+        longitud=${#binario};
+
+        suma=0;
+
+        for (( o=0; o<$longitud; o++ ))
+        do
+        cifra=${binario:$o:1}
+        opuesto=$(($longitud - o - 1))
+        
+        elevado=$((2**$opuesto))
+
+        suma=$(( $suma + $cifra * $elevado ))
+        done
+
+        ipfinal+=("$suma")
+    done
+
+    IFS=".";
+
+    echo "La IP $ip en decimal es => ${ipfinal[*]}";
+}
+
 
 echo "Que quieres hacer?"
 echo "1) Convertir a binario"
@@ -57,6 +146,8 @@ read op
 case $op in
 1) abinario;;
 2) adecimal;;
+3) ipabinario;;
+4) ipadecimal;;
 5) exit;;
 *) echo "Introduzca una opción correcta";;
 
