63f489c6f6348a4593a49c46463d035ea36ef234
Miguel Pérez <miguelperez@Beginners-Mac.local>
2025-03-27 14:14:09 +0100

    Creamos funcion para pasar a decimal y hago el menú

--- binario.sh
+++ binario.sh
@@ -5,7 +5,9 @@
 
 abinario() {
     read -p "Introduc el numero en decimal
-    " decimal
+" decimal
+
+    original=$decimal;
 
     res=0;
 
@@ -16,19 +18,46 @@ abinario() {
     binario="$sobrante$binario";
     done
 
-    echo "$decimal en Binario es: $binario";
+    echo "$original en Binario es: $binario";
 }
 
 adecimal() {
 
-read -p "Introduc el numero en binario
+    read -p "Introduc el numero en binario
 " binario
 
-longitud=${#binario};
+    longitud=${#binario};
 
-echo "$longitud";
 
+    suma=0;
+
+    for (( i=0; i<$longitud; i++ ))
+    do
+    cifra=${binario:$i:1}
+    opuesto=$(($longitud - i - 1))
+    
+    elevado=$((2**$opuesto))
+
+    suma=$(( $suma + $cifra * $elevado ))
+    done
+
+    echo "$binario convertido a decimal es: $suma";
 }
 
-adecimal
-abinario
+
+echo "Que quieres hacer?"
+echo "1) Convertir a binario"
+echo "2) Convertir a decimal"
+echo "3) Pasar IP de a binario"
+echo "4) Pasar IP a decimal"
+echo "5) Salir"
+read op
+
+
+case $op in
+1) abinario;;
+2) adecimal;;
+5) exit;;
+*) echo "Introduzca una opción correcta";;
+
+esac
